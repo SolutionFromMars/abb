6150a96cd4322a48dd44c9cb895b1a98f7c20b64
SolutionFromMars <93371811+SolutionFromMars@users.noreply.github.com>
2024-09-02 20:16:25 +0300

    Закончено взаимодействие с пользователями. Остались взаимодействия с базой данных и реализация удаления сообщений.

--- advblockbot.py
+++ advblockbot.py
@@ -1,25 +1,41 @@
-# Последняя проверка работы была в феврале 2024 года с aiogram 3.4.1 на python 3.11
+# Последняя проверка работы была в сентябре 2024 года с aiogram 3.12.0 на python 3.11
 import os
 if os.path.isfile('config.py'):
-    from config import TOKEN
+    from config import TOKEN # type: ignore
 else:
     with open('life.dat', 'w') as f:
         f.write(chr(0b10))
     input('Файл ключа config.py не найден\n')
     os._exit(-1)
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import BaseFilter
 from aiogram.enums import ParseMode
+from aiogram.filters.command import Command
 from asyncio import run
 import keyboard
 import sqlite3
+import time
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
+rt1 = Router()
+dp.include_router(rt1)
 
-db = sqlite3.connect('usage.db')
-crs = db.cursor()
-crs.execute("CREATE TABLE IF NOT EXISTS usage (chat_id INTEGER NOT NULL, calls_number INTEGER NOT NULL, status BOOLEAN NOT NULL CHECK (status IN (0, 1)))")
+def foo(func):
+    def wrapper(*args):
+        t = time.time()
+        func(*args)
+        print((time.time() - t) * 1000)
+    return(wrapper)
+
+@foo
+def Provoz(command: str):
+    con = sqlite3.connect('usage.db')
+    cur = con.cursor()
+    cur.execute(command)
+    con.commit()
+    cur.close()
+    con.close()
 
 def Restart():
     os.startfile('advblockbot.py')
@@ -29,7 +45,17 @@ def Restart():
 
 keyboard.add_hotkey('R+1+Home', Restart)
 
+rt1.message.filter(lambda m: m.chat.type != "private")
+
 try:
+    
+    t = time.time()
+    with sqlite3.connect('usage.db') as db:
+        db.cursor().execute("CREATE TABLE IF NOT EXISTS usage (chat_id INTEGER NOT NULL, calls_number INTEGER NOT NULL, status BOOLEAN NOT NULL CHECK (status IN (0, 1) ), UNIQUE (chat_id) )")
+    db.close()
+    print(int((time.time() - t) * 1000), 'миллисекунд')
+
+
     if os.path.isfile('life.dat'):
         with open('life.dat', 'r') as f:
             if f.read() == chr(0b01): print('Успешный перезапуск')
@@ -39,32 +65,26 @@ try:
     with open('life.dat', 'w') as f:
         f.write(chr(0b00))
 
-    def checkgroup(func): # Декоратор для сокращения кода основных командных асихронных функций
-        async def wrapper(m: types.Message):
-            if m.chat.type == 'group' or m.chat.type == 'supergroup':
-                await func(m)
-            else: await m.answer('Бот работает только в группах')
-        return wrapper
-
-    @dp.message(Command('start'))
-    @checkgroup
+    @rt1.message(Command('start'))
     async def NewChat(m: types.Message):
-        await m.answer('Я \- бот, который поможет избавляться от рекламы других ботов\. Поддержать можно по команде [/donate](/donate@advblockbot)', parse_mode=ParseMode.MARKDOWN_V2)
+        #Provoz(f'INSERT INTO usage VALUES ({}, {}, {})')
+        await m.answer('Я \- бот, который поможет избавляться от рекламы других ботов\. Поддержать можно по команде [/donate](/donate@advblockbot)', parse_mode = ParseMode.MARKDOWN_V2)
 
-    @dp.message(Command('off'))
-    @checkgroup
+    @rt1.message(Command('off'))
     async def Off(m: types.Message):
+        #Provoz('INSERT INTO usage ')
         await m.answer('Блокировка рекламы выключена')
 
-    @dp.message(Command('on'))
-    @checkgroup
+    @rt1.message(Command('on'))
     async def On(m: types.Message):
         await m.answer('Блокировка рекламы включена')
 
-    @dp.message(Command('donate'))
-    @checkgroup
+    @rt1.message(Command('donate'))
     async def Donate(m: types.Message):
-        pass
+        await m.answer('На данный момент донаты не принимаются')
+
+    @dp.message(lambda m: m.chat.type == "private", Command(commands = ['start', 'off', 'on', 'donate']))
+    async def OutReply(m: types.Message): await m.answer('Бот работает только в группах')
 
     async def main():
         await dp.start_polling(bot)
